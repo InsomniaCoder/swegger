7474e67f417bf60716c241ff56561e6cdf3aa3fe
Raksit Mantanacharu <raksit.m@ku.th>
2020-10-13 18:22:07 +0700

    🎉 init Spring project (#1)
    Co-authored-by: Raksit <rmanta1@ap.xom.com>

---
+++ src/test/java/org/nontechcompany/swegger/SweggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nontechcompany.swegger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
